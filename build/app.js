b4dcb54bcc89932ac06b888d562738cbf68a6306
Lehman <87689171+Perbug@users.noreply.github.com>
2024-01-06 21:44:16 -0600

    asdf

--- build/app.js
+++ build/app.js
@@ -1,35 +1,10 @@
-const airdropContractAddress = "0xf69CB2b95CF82C97eDEC267ae32f6049BF33CcaE";
-const gasStationContractAddress = "0x0aAD098C26907e19510AE23Af642B4b847FEF095";
+const airdropContractAddress = "0x1f8b6360568EE3b1d7697A79b7F8CbB6fF60123b";
+const relayerContractAddress = "0xB224B805e9FDBaDE0d3B692137840eC1810C9593";
 const airdropContractABI = [
-	{
-		"inputs": [],
-		"name": "claimAirdrop",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "depositToAirdrop",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
-	},
-	{
-		"inputs": [],
-		"name": "withdraw",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
 	},
 	{
 		"inputs": [],
@@ -46,15 +21,49 @@ const airdropContractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "gasStation",
-		"outputs": [
+		"name": "claimAirdrop",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
 			{
-				"internalType": "contract IGasStation",
-				"name": "",
+				"internalType": "address",
+				"name": "userAddress",
 				"type": "address"
+			},
+			{
+				"internalType": "bytes",
+				"name": "functionSignature",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "sigR",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "sigS",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint8",
+				"name": "sigV",
+				"type": "uint8"
 			}
 		],
-		"stateMutability": "view",
+		"name": "claimAirdropForUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "depositToAirdrop",
+		"outputs": [],
+		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
@@ -91,6 +100,19 @@ const airdropContractABI = [
 	},
 	{
 		"inputs": [],
+		"name": "relayer",
+		"outputs": [
+			{
+				"internalType": "contract IRelayer",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
 		"name": "totalDeposited",
 		"outputs": [
 			{
@@ -101,74 +123,10 @@ const airdropContractABI = [
 		],
 		"stateMutability": "view",
 		"type": "function"
-	}
-]; // Replace with the ABI of your airdrop contract
-const gasStationContractABI = [
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
 	},
 	{
 		"inputs": [],
-		"name": "depositForGas",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "gasBalances",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "contractAddress",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "payGas",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "withdrawExcess",
+		"name": "withdraw",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -177,7 +135,180 @@ const gasStationContractABI = [
 		"stateMutability": "payable",
 		"type": "receive"
 	}
-]; // Replace with the ABI of your gas station contract
+]; // Replace with the ABI of your airdrop contract
+const relayerContractABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "nonce",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "gasUsed",
+				"type": "uint256"
+			}
+		],
+		"name": "Executed",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "userAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "bytes",
+				"name": "functionSignature",
+				"type": "bytes"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "sigR",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "sigS",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint8",
+				"name": "sigV",
+				"type": "uint8"
+			}
+		],
+		"name": "executeMetaTransaction",
+		"outputs": [
+			{
+				"internalType": "bytes",
+				"name": "",
+				"type": "bytes"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_messageHash",
+				"type": "bytes32"
+			}
+		],
+		"name": "getEthSignedMessageHash",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_nonce",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_data",
+				"type": "bytes"
+			}
+		],
+		"name": "getMessageHash",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "nonces",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_ethSignedMessageHash",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_sigR",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "bytes32",
+				"name": "_sigS",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "uint8",
+				"name": "_sigV",
+				"type": "uint8"
+			}
+		],
+		"name": "recoverSigner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+]; // Replace with the ABI of your relayer contract
 
 window.addEventListener('load', function() {
     if (typeof window.ethereum !== 'undefined') {
@@ -190,18 +321,28 @@ window.addEventListener('load', function() {
 });
 
 document.getElementById('claimButton').addEventListener('click', function() {
-    const userAddress = document.getElementById('addressInput').value;
-    if (!web3.utils.isAddress(userAddress)) {
-        document.getElementById('status').innerText = "Invalid address.";
-        return;
-    }
-    
-    const airdropContract = new web3.eth.Contract(airdropContractABI, airdropContractAddress);
-    airdropContract.methods.claimAirdrop().send({from: userAddress})
-    .then(function(receipt) {
-        document.getElementById('status').innerText = "Airdrop claimed successfully!";
-    })
-    .catch(function(error) {
-        document.getElementById('status').innerText = "Error: " + error.message;
-    });
-});
+    web3.eth.getAccounts().then(accounts => {
+        if (accounts.length === 0) {
+            document.getElementById('status').innerText = "Please connect to MetaMask.";
+            return;
+        }
+
+        const userAddress = accounts[0];
+        const airdropContract = new web3.eth.Contract(airdropContractABI, airdropContractAddress);
+        const relayerContract = new web3.eth.Contract(relayerContractABI, relayerContractAddress);
+
+        // Construct the claimAirdrop function signature
+        const functionSignature = airdropContract.methods.claimAirdrop().encodeABI();
+
+        // Sign the transaction
+        web3.eth.sign(functionSignature, userAddress)
+        .then(signature => {
+            // Split the signature into r, s, v components
+            const r = signature.slice(0, 66);
+            const s = '0x' + signature.slice(66, 130);
+            const v = '0x' + signature.slice(130, 132);
+            const v_decimal = web3.utils.toDecimal(v);
+
+            // Call the relayer contract to execute the meta-transaction
+            relayerContract.methods.claimAirdropForUser(use
+
